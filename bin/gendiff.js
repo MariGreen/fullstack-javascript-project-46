2551becc84c43cf20cd4ed3fb6175fa21be05b65
Marina Grishina <marisupova@gmail.com>
2024-06-20 22:07:55 +0300

    first step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
+/* eslint-disable no-undef */
+import { program } from 'commander';
 
-console.log('i work');
+const genDiff = () => {
+
+  const name = 'gendiff';
+  const version = '1.0.0';
+
+  if (process.argv.includes('-h')) {
+    console.log(` 
+    Usage: ${name} [options]
+
+    Compares two configuration files and shows a difference.
+
+    Options:
+    -V, --version        output the version number
+    -h, --help           output usage information`)
+  } else if (process.argv.includes('-V')) {
+    console.log(version)
+  }
+};
+
+console.log('ну и чо');
+genDiff();
+
+export default genDiff;
